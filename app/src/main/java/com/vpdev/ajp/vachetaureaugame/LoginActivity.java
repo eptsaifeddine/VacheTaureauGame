dcce4f16ed3e253b4cbfa1358a258efa809bf7b1
saifeddine <ept.saifeddine@gmail.com>
2017-08-10 10:53:05 +0100

    version1

--- app/src/main/java/com/vpdev/ajp/vachetaureaugame/LoginActivity.java
+++ app/src/main/java/com/vpdev/ajp/vachetaureaugame/LoginActivity.java
@@ -2,12 +2,30 @@ package com.vpdev.ajp.vachetaureaugame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editText_email = (EditText)findViewById(R.id.editText_email);
+    private EditText editText_password=(EditText)findViewById(R.id.editText_password) ;
+    private Button button_login =(Button) findViewById(R.id.button_login) ;
+    private Button button_register =(Button)findViewById(R.id.button_register) ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/vpdev/ajp/vachetaureaugame/RegisterActivity.java
@@ -0,0 +1,22 @@
+package com.vpdev.ajp.vachetaureaugame;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDialog;
+
+/**
+ * Created by saifeddine on 10/08/17.
+ */
+
+public class RegisterActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
+
+
