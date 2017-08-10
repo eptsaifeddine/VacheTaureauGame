46ffd369573c5f1fd66e7aa8634fc80ab1f8a1b1
saifeddine <ept.saifeddine@gmail.com>
2017-08-10 13:08:35 +0100

    version

--- app/src/main/java/com/vpdev/ajp/vachetaureaugame/LoginActivity.java
+++ app/src/main/java/com/vpdev/ajp/vachetaureaugame/LoginActivity.java
@@ -13,6 +13,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.Toast;
 
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -28,7 +29,7 @@ public class LoginActivity extends AppCompatActivity {
     private EditText  editText_password ;
     private Button   button_login ;
     private Button button_register ;
-
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
         editText_email = (EditText)findViewById(R.id.editText_email);
         editText_password=(EditText)findViewById(R.id.editText_password) ;
         button_login =(Button) findViewById(R.id.button_login) ;
+        mAuth=FirebaseAuth.getInstance() ;
         button_register =(Button)findViewById(R.id.button_register) ;
         button_register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,7 +50,35 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        button_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+                sign_in() ;
+            }
+        });
+
+
+    }
+
+    private void sign_in() {
+        final String email=editText_email.getText().toString().trim() ;
+        final String password=editText_password.getText().toString().trim() ;
+
+        mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful())
+                    Toast.makeText(LoginActivity.this,"Logged in successfully",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(LoginActivity.this,"Operation failed",Toast.LENGTH_SHORT).show();
+
+
+
+
+            }
+        });
 
 
     }
