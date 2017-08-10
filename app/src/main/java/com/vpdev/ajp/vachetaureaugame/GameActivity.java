1635e65a74893f99687d497e73033ac52a024ca2
AbderrahmenMS <masmoudi.rhouma@gmail.com>
2017-08-10 15:13:21 +0100

    version5

--- app/src/main/java/com/vpdev/ajp/vachetaureaugame/GameActivity.java
+++ app/src/main/java/com/vpdev/ajp/vachetaureaugame/GameActivity.java
@@ -4,18 +4,28 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by saifeddine on 10/08/17.
  */
 
 public class GameActivity  extends AppCompatActivity {
-    private Button button
+    private Button button_vt;
+    private EditText edit_vt;
+    private Button button_number;
+    private EditText edit_number;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
+        button_vt = (Button)findViewById(R.id.button_submit_vt);
+        button_number = (Button)findViewById(R.id.button_submit_number);
+        edit_number = (EditText)findViewById(R.id.editText_number);
+        edit_vt = (EditText)findViewById(R.id.editText_vt);
+
+
 
     }
 
