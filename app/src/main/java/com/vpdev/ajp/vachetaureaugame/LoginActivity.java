bf65e02e0cdbe7c1d885f368ac37890658be7fbf
saifeddine <ept.saifeddine@gmail.com>
2017-08-10 11:21:05 +0100

    version2

--- app/src/main/java/com/vpdev/ajp/vachetaureaugame/LoginActivity.java
+++ app/src/main/java/com/vpdev/ajp/vachetaureaugame/LoginActivity.java
@@ -1,25 +1,49 @@
 package com.vpdev.ajp.vachetaureaugame;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText editText_email = (EditText)findViewById(R.id.editText_email);
-    private EditText editText_password=(EditText)findViewById(R.id.editText_password) ;
-    private Button button_login =(Button) findViewById(R.id.button_login) ;
-    private Button button_register =(Button)findViewById(R.id.button_register) ;
+    private EditText  editText_email ;
+    private EditText  editText_password ;
+    private Button   button_login ;
+    private Button button_register ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editText_email = (EditText)findViewById(R.id.editText_email);
+        editText_password=(EditText)findViewById(R.id.editText_password) ;
+        button_login =(Button) findViewById(R.id.button_login) ;
+        button_register =(Button)findViewById(R.id.button_register) ;
         button_register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                Intent intent =new Intent(LoginActivity.this,RegisterActivity.class);
+                LoginActivity.this.startActivity(intent);
+
 
             }
         });
--- app/src/main/java/com/vpdev/ajp/vachetaureaugame/RegisterActivity.java
+++ app/src/main/java/com/vpdev/ajp/vachetaureaugame/RegisterActivity.java
@@ -1,22 +1,47 @@
 package com.vpdev.ajp.vachetaureaugame;
 
-import android.os.Bundle;
-import android.support.v7.app.ActionBar;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.app.AppCompatDialog;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.MultiAutoCompleteTextView;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Created by saifeddine on 10/08/17.
  */
 
 public class RegisterActivity extends AppCompatActivity{
-
-
+    private Button button_save=(Button) findViewById(R.id.button_save) ;
+    private EditText editText_register_email=(EditText)findViewById(R.id.editText_register_email);
+    private EditText editText_register_name=(EditText)findViewById(R.id.editText_register_name);
+    private EditText editText_register_password=(EditText)findViewById(R.id.editText_register_password);
+    private FirebaseAuth mAuth ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+
+
+
+
 }
 
 
