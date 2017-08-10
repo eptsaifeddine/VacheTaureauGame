f5568dc908ed3aa642a469e932df5a8535bca492
saifeddine <ept.saifeddine@gmail.com>
2017-08-10 16:42:33 +0100

    version

--- app/src/main/java/com/vpdev/ajp/vachetaureaugame/GameActivity.java
+++ app/src/main/java/com/vpdev/ajp/vachetaureaugame/GameActivity.java
@@ -3,14 +3,22 @@ package com.vpdev.ajp.vachetaureaugame;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -24,41 +32,97 @@ public class GameActivity  extends AppCompatActivity {
     private EditText edit_number;
     private DatabaseReference databaseReference ;
     private FirebaseAuth firebaseAuth ;
-
+        public static int gamefound ;
+        public static String gameId ;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
 
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_layout);
-        button_vt = (Button)findViewById(R.id.button_submit_vt);
-        button_number = (Button)findViewById(R.id.button_submit_number);
-        edit_number = (EditText)findViewById(R.id.editText_number);
-        databaseReference=FirebaseDatabase.getInstance().getReference() ;
-        firebaseAuth=FirebaseAuth.getInstance() ;
+        button_vt = (Button) findViewById(R.id.button_submit_vt);
+        button_number = (Button) findViewById(R.id.button_submit_number);
+        edit_number = (EditText) findViewById(R.id.editText_number);
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("games");
+        firebaseAuth = FirebaseAuth.getInstance();
+        edit_vt = (EditText) findViewById(R.id.editText_vt);
+    if (MenuActivity.player_state.equals("host"))
+    {
+        if (gamefound!=0)
+        {
 
-        edit_vt = (EditText)findViewById(R.id.editText_vt);
-        button_vt.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+    else if (MenuActivity.player_state.equals("client"))
+    {
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View view) {
-                Random r = new Random();
-                int i = r.nextInt(10000 - 65) + 65;
-                databaseReference.child("game"+Integer.toString(i)).child("host id").setValue(firebaseAuth.getCurrentUser().getUid());
-                databaseReference.child("game"+Integer.toString(i)).child("client id").setValue("0");
-                databaseReference.child("game"+Integer.toString(i)).child("VT").setValue("NOTSTARTEDYET");
-                databaseReference.child("game"+Integer.toString(i)).child("NUMBER").setValue("NOTSTARTEDYET");
-//
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                HashMap<String,HashMap<String,String>> l =(HashMap) dataSnapshot.getValue() ;
+
+                Toast.makeText(GameActivity.this,Integer.toString(l.size()),Toast.LENGTH_SHORT).show() ;
+                Log.v("aaaaaaaaaaaaa","the data "+l) ;
+                          if (gamefound==0)
+                       for (HashMap.Entry<String, HashMap<String,String>> entry : l.entrySet()) {
+                           String key = entry.getKey();
+                            HashMap<String,String> hashmap = entry.getValue();
+                              Log.v("aaaaaaaaaaaaa","the data "+key) ;             
+
+                        if (hashmap.get("client id").equals("0"))
+                        {
+                            gameId=key;
+                            gamefound=1 ;
+                            databaseReference.child(gameId).child("client id").setValue(firebaseAuth.getCurrentUser().getUid()) ;
+                            break ;
+                        }
 
 
+                           // Toast.makeText(GameActivity.this,it.toString(),Toast.LENGTH_SHORT).show() ;
 
+                       }
 
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
             }
         });
 
 
+
+
+
+
     }
+    else
+        Toast.makeText(GameActivity.this,"Error",Toast.LENGTH_SHORT).show();
+
+    }
+
+
+
+
+
 
 
 
--- app/src/main/java/com/vpdev/ajp/vachetaureaugame/MenuActivity.java
+++ app/src/main/java/com/vpdev/ajp/vachetaureaugame/MenuActivity.java
@@ -1,20 +1,83 @@
 package com.vpdev.ajp.vachetaureaugame;
 
-import android.os.Bundle;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Random;
+
 
 /**
  * Created by saifeddine on 10/08/17.
  */
 
 public class MenuActivity extends AppCompatActivity {
-
+private Button button_create ;
+private Button button_find ;
+private DatabaseReference databaseReference ;
+private FirebaseAuth firebaseAuth ;
+public static  String player_state ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+        databaseReference= FirebaseDatabase.getInstance().getReference().child("games");
+        firebaseAuth=FirebaseAuth.getInstance() ;
+        button_find=(Button) findViewById(R.id.button_findgame ) ;
+
+        button_create =(Button)findViewById(R.id.button_creategame) ;
+        button_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random r = new Random();
+                int i = r.nextInt(10000 - 65) + 65;
+                databaseReference.child("game"+Integer.toString(i)).child("host id").setValue(firebaseAuth.getCurrentUser().getUid());
+                databaseReference.child("game"+Integer.toString(i)).child("client id").setValue("0");
+                databaseReference.child("game"+Integer.toString(i)).child("VT").setValue("NOTSTARTEDYET");
+                databaseReference.child("game"+Integer.toString(i)).child("NUMBER").setValue("NOTSTARTEDYET");
+                player_state="host" ;
+                Intent intent =new Intent(MenuActivity.this,GameActivity.class) ;
+                MenuActivity.this.startActivity(intent);
+
+
+
+
+
+
+            }
+        });
+        button_find.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                player_state="client" ;
+                Intent intent =new Intent(MenuActivity.this,GameActivity.class) ;
+                MenuActivity.this.startActivity(intent);
+
+
+
+
+            }
+        });
+
+
 
     }
 
 
 }
+
+
+
